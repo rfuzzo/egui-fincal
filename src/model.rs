4d2c4d413f661fcf255ae55ec3a337a2080a7d13
rfuzzo <r.fuzzo@gmail.com>
2023-01-05 18:32:41 +0100

    feat: export button
    remove csv crate

--- src/model.rs
+++ src/model.rs
@@ -1,4 +1,5 @@
 use chrono::NaiveDate;
+use std::{fmt, str::FromStr};
 
 #[derive(serde::Deserialize, serde::Serialize, Clone, PartialEq)]
 //#[serde(default)]
@@ -13,4 +14,50 @@ pub struct FinItem {
     // viewmodel
     #[serde(skip)]
     pub(crate) editable: bool,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct ParseFinItemError;
+
+// ToStr as csv
+impl fmt::Display for FinItem {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "{};{};{};{};{};{}",
+            self.date, self.item, self.category, self.price, self.owner, self.ratio
+        )
+    }
+}
+
+// FromStr as csv
+impl FromStr for FinItem {
+    type Err = ParseFinItemError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let splits: Vec<&str> = s.split(';').collect();
+        if splits.len() != 6 {
+            return Err(ParseFinItemError);
+        }
+
+        let d_fromstr = splits[0]
+            .parse::<NaiveDate>()
+            .map_err(|_| ParseFinItemError)?;
+        let i_fromstr = splits[0].parse::<String>().map_err(|_| ParseFinItemError)?;
+        let c_fromstr = splits[0].parse::<String>().map_err(|_| ParseFinItemError)?;
+        let p_fromstr = splits[0].parse::<f32>().map_err(|_| ParseFinItemError)?;
+        let o_fromstr = splits[0].parse::<String>().map_err(|_| ParseFinItemError)?;
+        let r_fromstr = splits[0].parse::<f32>().map_err(|_| ParseFinItemError)?;
+
+        Ok(FinItem {
+            date: d_fromstr,
+            item: i_fromstr,
+            category: c_fromstr,
+            price: p_fromstr,
+            owner: o_fromstr,
+            ratio: r_fromstr,
+            // todo
+            editable: false,
+        })
+    }
 }
--- src/views/top_panel_view.rs
+++ src/views/top_panel_view.rs
@@ -1,5 +1,10 @@
 use futures::executor::block_on;
 use log::warn;
+use rfd::FileDialog;
+use std::{
+    fs::File,
+    io::{BufRead, BufReader, BufWriter, Write},
+};
 
 use crate::{model::FinItem, TemplateApp};
 
@@ -17,17 +22,11 @@ pub(crate) fn show(ui: &mut egui::Ui, _frame: &mut eframe::Frame, app: &mut Temp
                         .await;
 
                     let data = file_option.unwrap().read().await;
-                    let reader = std::io::BufReader::new(data.as_slice());
-                    let mut rdr = csv::Reader::from_reader(reader);
-                    for (i, result) in rdr.deserialize().enumerate() {
-                        let record: Result<FinItem, csv::Error> = result;
-                        match record {
-                            Ok(item) => {
-                                app.items.push(item);
-                            }
-                            Err(_) => {
-                                warn!("Failed to parse line {i}");
-                            }
+                    for (i, line) in BufReader::new(data.as_slice()).lines().enumerate() {
+                        if let Some(item) = parse_line(line) {
+                            app.items.push(item);
+                        } else {
+                            warn!("Failed to parse line {}", i)
                         }
                     }
                 };
@@ -38,7 +37,25 @@ pub(crate) fn show(ui: &mut egui::Ui, _frame: &mut eframe::Frame, app: &mut Temp
             // Export button
             #[cfg(not(target_arch = "wasm32"))] // no File->Export on web pages!
             if ui.button("Export Data").clicked() {
-                // TODO
+                let some_path = FileDialog::new()
+                    .add_filter("csv", &["csv"])
+                    .set_directory("/")
+                    .save_file();
+
+                if let Some(path) = some_path {
+                    if let Ok(file) = File::create(path.as_path()) {
+                        let mut fw = BufWriter::new(file);
+                        for i in app.items.iter() {
+                            let str = i.to_string();
+                            match fw.write(str.as_bytes()) {
+                                Ok(_) => {}
+                                Err(e) => warn!("Failed to write line {}", e),
+                            }
+                        }
+                    } else {
+                        todo!()
+                    }
+                }
             }
 
             // Quit button
@@ -55,4 +72,21 @@ pub(crate) fn show(ui: &mut egui::Ui, _frame: &mut eframe::Frame, app: &mut Temp
             egui::warn_if_debug_build(ui);
         });
     });
+}
+
+fn parse_line(line: Result<String, std::io::Error>) -> Option<FinItem> {
+    match line {
+        Ok(s) => {
+            if let Ok(item) = s.as_str().parse::<FinItem>() {
+                Some(item)
+            } else {
+                warn!("Failed to parse line");
+                None
+            }
+        }
+        Err(err) => {
+            warn!("Failed to read line {}", err);
+            None
+        }
+    }
 }
